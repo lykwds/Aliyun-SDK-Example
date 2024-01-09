60fe97e5bc4ec402dabb08121414b3c94f44dfa1
luyk <116431700+lykwds@users.noreply.github.com>
2024-01-09 10:37:42 +0800

    Update 获取一个region下的所有实例ID.py

--- ECS/获取一个region下的所有实例ID.py
+++ ECS/获取一个region下的所有实例ID.py
@@ -1,19 +1,33 @@
 from aliyunsdkcore.client import AcsClient
-from aliyunsdkcore.acs_exception.exceptions import ClientException
-from aliyunsdkcore.acs_exception.exceptions import ServerException
 from aliyunsdkecs.request.v20140526 import DescribeInstancesRequest
+import json
 
-# 创建一个AcsClient实例
-client = AcsClient(
-   "<your-access-key-id>", 
-   "<your-access-key-secret>",
-   "cn-hangzhou")
+def get_instances(client, page_number):
+    request = DescribeInstancesRequest.DescribeInstancesRequest()
+    request.set_accept_format('json')
+    request.set_PageSize(50)  # 每页实例数量
+    request.set_PageNumber(page_number)  # 第几页
 
-# 创建一个DescribeInstancesRequest实例
-request = DescribeInstancesRequest.DescribeInstancesRequest()
 
-# 发送请求并获取response
-response = client.do_action_with_exception(request)
+    response = client.do_action_with_exception(request)
+    json_response = json.loads(str(response, encoding='utf-8'))
 
-# 从response中抽取实例ID
-instanceIds = [instance["InstanceId"] for instance in response["Instances"]["Instance"]]
+    instanceIDs = [instance["InstanceId"] for instance in json_response["Instances"]["Instance"]]
+    return json_response["TotalCount"], instanceIDs
+
+
+client = AcsClient('<your-access-key-id>', '<your-access-key-secret>', 'cn-hangzhou')
+
+all_instanceIDs = []
+page_number = 1
+while True:
+    total_count, instanceIDs = get_instances(client, page_number)
+    all_instanceIDs += instanceIDs
+    # 如果已经获取了所有实例，则退出循环
+    if len(all_instanceIDs) >= total_count:
+        break
+    # 否则，获取下一页的数据
+    else:
+        page_number += 1
+
+print(all_instanceIDs)
